217cb9c1463ae04706898f270c107aa05f711518
Sajjad Alsaffar <109440699+Sra1995@users.noreply.github.com>
2025-01-21 21:11:42 -0500

    Update topKFrequent.py

--- Top K Frequent/topKFrequent.py
+++ Top K Frequent/topKFrequent.py
@@ -11,3 +11,18 @@ class Solution:
             result.append(heapq.heappop(heap)[1]) # so much heap heap heap heap
 
         return result
+
+# OR this solutions but heaping is more optimal
+# class Solution:
+#     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+#         freq_dict = Counter(nums)
+
+#         arr = []
+#         for num, cnt in freq_dict.items():
+#             arr.append([cnt, num])
+#         arr.sort()
+
+#         result = []
+#         while len(result) < k:
+#             result.append(arr.pop()[1])
+#         return result
